a95f5592580cb6d83068b249546ad8949459831d
Adeyemo Oluwadamilare Emmanuel <vinceoludare@gmail.com>
2024-10-02 05:02:40 +0100

    Initial commit

---
+++ packages/backend/routes/authenticationRoutes.js
@@ -0,0 +1,19 @@
+import { Router } from "express";
+import tokenVerification from "../security/authentication.js";
+import login from "../controllers/authentication/login.js";
+import loadProfile from "../controllers/access/controls/loadProfile.js";
+import logout from "../controllers/authentication/logout.js";
+import register from "../controllers/authentication/register.js";
+import { createRequire } from 'module';
+
+const require = createRequire(import.meta.url);
+
+const authRoute = Router();
+
+authRoute.post("/register", (req, res) => register(req, res));
+authRoute.post("/login", (req, res) => login(req, res));
+authRoute.post("/logout", (req, res) => logout(req, res));
+
+authRoute.get("/users", tokenVerification, (req, res) => loadProfile(req, res));
+
+export default authRoute;
